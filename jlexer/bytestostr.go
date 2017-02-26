5d608ccfff4bf40c244150ff479c865779073608
Long Hoang <long@mindworker.de>
2017-02-26 21:18:53 +0800

    Add documentation related to previously added build tags to disable unsafe

--- jlexer/bytestostr.go
+++ jlexer/bytestostr.go
@@ -1,3 +1,7 @@
+// This file will only be included to the build if neither
+// easyjson_nounsafe nor appengine build tag is set. See README notes
+// for more details.
+
 //+build !easyjson_nounsafe
 //+build !appengine
 
--- jlexer/bytestostr_nounsafe.go
+++ jlexer/bytestostr_nounsafe.go
@@ -1,3 +1,6 @@
+// This file is included to the build if any of the buildtags below
+// are defined. Refer to README notes for more details.
+
 //+build easyjson_nounsafe appengine
 
 package jlexer
