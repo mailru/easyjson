81f2536554cdb7be3703929da4d7297acd4f5b50
Aleksandr Razumov <ar@gortc.io>
2019-08-30 12:51:08 +0300

    better track tool dependencies

--- benchmark/tools.go
+++ benchmark/tools.go
@@ -1,6 +1,8 @@
 //+build tools
 
-package benchmark
+// Package tools tracks dependencies on binaries not otherwise referenced in the codebase.
+// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+package tools
 
 import (
 	_ "github.com/ugorji/go/codec/codecgen"
