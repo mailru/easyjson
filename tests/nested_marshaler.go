5c65c6fc0ee649970ae2aeff20dc0bb6a24aff6b
Alexandr Mayorskiy <a.mayorskiy@corp.mail.ru>
2020-07-14 17:28:28 +0300

    add test with nested marshaler interface

---
+++ tests/nested_marshaler.go
@@ -0,0 +1,31 @@
+package tests
+
+import (
+	"github.com/mailru/easyjson"
+	"github.com/mailru/easyjson/jlexer"
+	"github.com/mailru/easyjson/jwriter"
+)
+
+type MarshalerUnmarshaler interface {
+	easyjson.Marshaler
+	easyjson.Unmarshaler
+}
+
+//easyjson:json
+type NestedMarshaler struct {
+	Value MarshalerUnmarshaler
+	Value2 int
+}
+
+type StructWithMarshaler struct {
+	Value int
+}
+
+func (s *StructWithMarshaler) UnmarshalEasyJSON(w *jlexer.Lexer) {
+	s.Value = w.Int()
+}
+
+func (s *StructWithMarshaler) MarshalEasyJSON(w *jwriter.Writer) {
+	w.Int(s.Value)
+}
+
